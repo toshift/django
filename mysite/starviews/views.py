4b4709f6e54e780f1a271b2451d061e9d8941b7b
yamauchi toshikazu <yamauchi@JIG-SAW.COM>
2016-05-24 17:52:01 +0900

    test

--- mysite/starviews/views.py
+++ mysite/starviews/views.py
@@ -1,4 +1,6 @@
 from django.shortcuts import render
 
 def starview(request):
-    return render(request, 'starviews/srarview.html', dict(form=form, book_id=book_id))
+    return render(request,
+                  'starviews/starview.html'    # 使用するテンプレート
+                 )               # テンプレートに渡すデータ

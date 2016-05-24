5810738510ae0e888b5033fe8a1e28d4851a0a48
yamauchi toshikazu <yamauchi@JIG-SAW.COM>
2016-05-24 12:00:43 +0900

    ブックリストの追加

--- mysite/bootstview/views.py
+++ mysite/bootstview/views.py
@@ -2,12 +2,16 @@
 
 from django.shortcuts import render
 from django.http import HttpResponse
+from bootstview.models import Book
 
 
 def book_list(request):
     """"書籍の一覧"""
-    return HttpResponse('書籍の一覧')
-
+#    return HttpResponse('書籍の一覧')
+    book = Book.objects.all().order_by('id')
+    return render(request,
+                  'bootstview/book_list.html',    # 使用するテンプレート
+                  {'books': books})               # テンプレートに渡すデータ
 
 def book_edit(request, book_id=None):
     """書籍の編集"""

13c117fefc121285249beeb8d39197fa57668eee
yamauchi toshikazu <yamauchi@JIG-SAW.COM>
2016-05-24 14:11:12 +0900

    削除処理の追加

--- mysite/bootstview/views.py
+++ mysite/bootstview/views.py
@@ -36,4 +36,8 @@ def book_edit(request, book_id=None):
 
 def book_del(request, book_id):
     """書籍の削除"""
-    return HttpResponse('書籍の削除')
+#    return HttpResponse('書籍の削除')
+    book = get_object_or_404(Book, pk=book_id)
+    book.delete()
+    return redirect('bootstview:book_list')
+
